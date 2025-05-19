cb0aa0de6e30b87acf8ef9e1e2d436eb78e1ff3f
Briseyda Arbizu <briseyda.arbizu.valladolid@vallegrande.edu.pe>
2025-05-19 01:19:03 +0000

    feat: add update reservation interface

---
+++ src/app/feature/reservation/reservation-form/reservation-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-form',
+  imports: [],
+  templateUrl: './reservation-form.component.html',
+  styleUrl: './reservation-form.component.scss'
+})
+export class ReservationFormComponent {
+
+}

adb74b4c3cebedd9b5f1dda8a5d3769ed118ac8b
victor <victor.torreblanca.franco@vallegrande.edu.pe>
2025-05-20 20:12:32 -0500

    feat: created two components  'experience-card' in shared/components and 'experiences' in pages

---
+++ src/app/pages/experiences/experiences.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences',
+  imports: [],
+  templateUrl: './experiences.component.html',
+  styleUrl: './experiences.component.scss'
+})
+export class ExperiencesComponent {
+
+}
---
+++ src/app/shared/components/experience-card/experience-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-card',
+  imports: [],
+  templateUrl: './experience-card.component.html',
+  styleUrl: './experience-card.component.scss'
+})
+export class ExperienceCardComponent {
+
+}

c5a01c4da027549016bfaf8e45217b5a53b990d3
ordonezL1 <luis.ordonez.lopez@vallegrande.edu.pe>
2025-06-25 11:37:55 -0500

    style(contact-us): add import and icons to form

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true, // si usas standalone components
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })

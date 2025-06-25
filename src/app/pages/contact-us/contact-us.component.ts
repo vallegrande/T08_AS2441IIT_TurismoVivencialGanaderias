7e466987803fe595b146ada06574bc2e631a79d6
ordonezL1 <luis.ordonez.lopez@vallegrande.edu.pe>
2025-06-25 12:25:30 -0500

    feat(contact-us): add validations to form

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  standalone: true, // si usas standalone components
-  imports: [CommonModule, MatIconModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatIconModule,
+    ReactiveFormsModule 
+  ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit, OnDestroy {
+  contactForm!: FormGroup;
+
   images = [
     '/images/contact-us/imgContactForm01.jpg',
     '/images/contact-us/imgContactForm02.jpg',
@@ -18,7 +25,18 @@ export class ContactUsComponent {
   currentIndex = 0;
   intervalId: any;
 
+  constructor(private fb: FormBuilder) {}
+
   ngOnInit(): void {
+    // Para el formulario
+    this.contactForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.pattern(/^([A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)(\s[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+){0,2}$/)]],
+      lastName: ['', [Validators.required, Validators.pattern(/^([A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)(\s[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+){0,2}$/)]],
+      phone: ['', [Validators.required, Validators.pattern(/^[0-9]{9}$/)]],
+      message: ['', [Validators.required, Validators.maxLength(250)]]
+    });
+
+    // Para el carrusel
     this.intervalId = setInterval(() => {
       this.currentIndex = (this.currentIndex + 1) % this.images.length;
     }, 4000);
@@ -26,5 +44,14 @@ export class ContactUsComponent {
 
   ngOnDestroy(): void {
     clearInterval(this.intervalId);
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('Formulario enviado:', this.contactForm.value);
+      
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
   }
 }

2a212db429569cf05cec21e00374b5753dd441bc
Yon Rojas <yon.rojas@vallegrande.edu.pe>
2025-06-25 11:46:01 -0500

    feat: improve experiences detail page with enhanced design and features

--- src/app/feature/reservation/reservation-form/reservation-form.component.ts
+++ src/app/feature/reservation/reservation-form/reservation-form.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-form',
-  imports: [],
   templateUrl: './reservation-form.component.html',
-  styleUrl: './reservation-form.component.scss'
+  styleUrl: './reservation-form.component.scss',
+  standalone: true,
+  imports: []
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+  experienceId!: number;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.experienceId = +params['id'];
+      console.log('ID recibido en formulario de reserva:', this.experienceId);
+      // Aquí podrías cargar info extra con el ID si lo necesitas
+    });
+  }
 }
